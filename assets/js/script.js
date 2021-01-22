0977c6c685d2b05f8c502477ee69591db7089e23
Vicky Loebel <vickyloebel@gmail.com>
2021-01-22 14:33:18 -0700

    tried openweatherforecast but that's paid

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,85 +2,73 @@
 
 const openWeatherAPI = "http://api.openweathermap.org/data/2.5/weather?q="
 const openWeatherOneCallAPI = "https://api.openweathermap.org/data/2.5/onecall?"
-const excludeString ="&exclude= minutely, hourly, alerts"
+const openWeatherForecastAPI = "https://api.openweathermap.org/data/2.5/forecast/daily?q="
+const excludeString = "&exclude=minutely,hourly,daily,alerts"
 const weatherUnits = "&units=imperial"
-const apiKey= "&appid=47cc7111aeaa92ded720903e4f89338c"
-
-
+const apiKey = "&appid=47cc7111aeaa92ded720903e4f89338c"
+const iconURL = "http://openweathermap.org/img/wn/"
 
 // ***********************************************
-// Weather Forecast from  https://openweathermap.org/guide
-// username: vk APIkey: 47cc7111aeaa92ded720903e4f89338c
-//
-function myFunction() {
+// APIkey username vk
+// ***********************************************
+function getWeatherData() { 
   var searchCity = document.getElementById('searchCity').value;
   // Get current weather conditions
   // and the lat long of the indicated city
+
   fetch(
     openWeatherAPI +
+    searchCity +
+    weatherUnits +
+    apiKey
+
+  ).then(function (resp) { 
+    return resp.json();
+
+  }).then(function (data) { 
+    // extract longitude and latitude from first call
+    var longitude = data.coord.lon;
+    var latitude = data.coord.lat;
+    // then use OneCallAPI for everything else
+    fetch(openWeatherOneCallAPI +
+      "&lat=" + latitude +
+      "&lon=" + longitude +
+      weatherUnits +
+      excludeString +
+      apiKey
+
+    ).then(function (resp2) { 
+      return resp2.json();
+
+    }).then(function (data) { 
+      console.log(`oneCallData`);
+      console.log(data);
+      // get the bits we want
+      console.log(`temp = ${data.current.temp}`);
+      console.log(`humidity = ${data.current.humidity}`);
+      console.log(`UV Index = ${data.current.uvi}`);
+
+      console.log(`wind speed = ${data.current.wind_speed}`);
+
+    });
+    // now get the forecast for 5 days.
+    // question - excluding today? Doc is unclear
+    fetch(
+      openWeatherForecastAPI +
       searchCity +
+      "&cnt=5" +
       weatherUnits +
       apiKey
-  )
-    .then(function (resp) {
-      return resp.json()
-    }) 
-    .then(function (data){ 
+  
+    ).then(function (resp3) {
+      return resp3.json();
+  
+    }).then(function (data) {
+      console.log(`forecastData`);
       console.log(data);
-      console.log(`temp = ${data.main.temp}`);
-      console.log(`humidity = ${data.main.humidity}`);
-      console.log(`wind speed = ${data.wind.speed}`);
-      var longitude = data.coord.lon;
-      var latitude = data.coord.lat;
-      // console.log(`uv index = ${data.main.temp}`)
-      // get the uv index and other stuff we need
-      // from the oneCallAPI
-      fetch(openWeatherOneCallAPI +
-        "&lat=" + latitude +
-        "&lon=" + longitude +
-        excludeString +
-        apiKey
-        )
-        .then(function (oneCallData) {
-          return oneCallData.json()
-        })
-        .then(function (ocd) {
-          console.log(ocd)
-      })
     })
-    .catch(function() {
-      // catch any errors
-    });
-  }
-  
-   
-  
-  
 
+  });
 
-  // .then(function(res) {
-  //   console.log(`second results: ${res}`);
-  // .then(function(response) {
-  //   console.log(response);
-  //  })
+}
 
-  // console.log(`getting forecast for ${searchCity}`);
-  // fetch(
-  //   `http://api.openweathermap.org/data/2.5/weather?q= 
-  //     ${searchCity}
-  //     &units=imperial
-  //     &appid=47cc7111aeaa92ded720903e4f89338c`
-  // )
-    // .then(function(response) {
-    //   return response.json();
-    // })
-    // .then(function(response) {
-    //   console.log(response.data[0]);
-    //   var responseContainerEl = document.querySelector('#response-container');
-    //   responseContainerEl.innerHTML = '';
-    //   var gifImg = document.createElement('img');
-    //   gifImg.setAttribute('src', response.data[0].images.fixed_height.url);
-    //   responseContainerEl.appendChild(gifImg);
-    // });
- 
-// }
