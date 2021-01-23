aed981092151d0ddd6e3d30b0e328eae87ef28a4
Vicky Loebel <vickyloebel@gmail.com>
2021-01-23 10:24:20 -0700

    added current weather display values

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,12 @@ const weatherUnits = "&units=imperial"
 const apiKey = "&appid=47cc7111aeaa92ded720903e4f89338c"
 const iconURL = "http://openweathermap.org/img/wn/"
 
+const testIcon="http://openweathermap.org/img/wn/10d@2x.png"
 // ***********************************************
 // APIkey username vk
 // ***********************************************
 function getWeatherData() { 
-  var searchCity = document.getElementById('searchcity').value;
+  searchCity = document.getElementById('searchcity').value;
   // Get current weather conditions
   // and the lat long of the indicated city
 
@@ -42,21 +43,50 @@ function getWeatherData() {
       return resp2.json();
 
     }).then(function (data) {
-      console.log(`today`);
-      // get the bits we want
+      //now we have all our data and can start to display
+      console.log(data);
+      var searchCity = document.getElementById('searchcity').value;
+      //  selected city
+      var displayCityEl = document.getElementById('display-city');
+      displayCityEl.textContent = searchCity;
+      //  today's date
+      var dateTodayEl = document.getElementById("date-today");
+      dateTodayEl.textContent = ` (${today}) `;
+      //  weather icon
+      var cwIconEl = document.getElementById("current-weather-icon");
+      cwIconEl.setAttribute("src", `${iconURL}` + `${data.current.weather[0].icon}.png`);
+      //  temperature
+      var cTempEl = document.getElementById("current-temp");
+      cTempEl.textContent = data.current.temp;
+      // humidity
+      var cHumidityEl = document.getElementById("current-humidity");
+      cHumidityEl.textContent = data.current.humidity;
+      // wind speed
+      var cWindSpeedEl = document.getElementById("current-wind-speed");
+      cWindSpeedEl.textContent = data.current.wind_speed;
+      // UV Index
+      var cUViEl = document.getElementById("current-uvi");
+      cUViEl.textContent = data.current.uvi;
+
+
+      
+     // get the bits we want
       console.log(`temp = ${data.current.temp}`);
       console.log(`humidity = ${data.current.humidity}`);
       console.log(`UV Index = ${data.current.uvi}`);
       console.log(`wind speed = ${data.current.wind_speed}`);
+      console.log(`icon is = ${data.current.weather[0].icon}`);
 
       // get the forecast
-      console.log(`5-DAY FORECAST`);
-      for (var i = 1; i < 5; i++) { 
-        console.log(`----DAY--- ${i}:`);
-        console.log(`clouds ${data.daily[i].clouds}:`);
-        console.log(`temp ${data.daily[i].temp.day}:`);
-        console.log(`humidity ${data.daily[i].humidity}:`);
-      }
+      // console.log(`5-DAY FORECAST`);
+      // for (var i = 1; i < 5; i++) { 
+      //   console.log(`----DAY--- ${i}:`);
+      //   console.log(`clouds ${data.daily[i].clouds}:`);
+      //   console.log(`temp ${data.daily[i].temp.day}:`);
+      //   console.log(`humidity ${data.daily[i].humidity}:`);
+      //   console.log(`icon is = ${data.daily[i].weather[0].icon}:`);
+
+      // }
     });
   });
 }
@@ -73,17 +103,12 @@ for (var i = 1; i <= 5; i++) {
   console.log(`${new_date}`);
 
 }
-// var searchCityEl = document.getElementById("search-city");
-// var searchCity = searchCityEl.value;
-// var searchCity = document.getElementById('search-city').value;
-// console.log(`search-city ${searchCity}`);
-// // searchCityEl.textContent = searchCity;
 
-// var dateTodayEl = document.getElementById("date-today");
-// dateTodayEl.textContent = ` (${today}) `;
+getWeatherData();
 
-// var displayCityEl = document.getElementById("display-city");
-// displayCityEl.textContent = searchCity;
+
+
+
 
 // decrementEl.addEventListener('click', function() {
 //   if (count > 0) {
@@ -91,3 +116,4 @@ for (var i = 1; i <= 5; i++) {
 //     setCounterText();
 //   }
 // });
+
