18afce60271339723a039eab7b44d7a76962f1ca
Vicky Loebel <vickyloebel@gmail.com>
2021-01-24 16:30:18 -0700

    added handling for bad city name

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,11 +13,12 @@ const iconURL = "http://openweathermap.org/img/wn/"
 var searchCityList = [];
 var lastSearch = "TUCSON";
 const lastSearchDefault = "TUCSON";
+var cityNameOK = true;
 
 
 //////////////////////////////////////////
 // FUNCTION saveSearchHistory() 
-function saveSearchHistory () {
+function saveSearchHistory() {
   localStorage.setItem("lastSearch", lastSearch);
   localStorage.setItem("searchCityList", JSON.stringify(searchCityList));
 }
@@ -35,7 +36,7 @@ function loadSearchHistory() {
   //   lastSearch = lastSearchDefault;
   // } 
   searchCityList = JSON.parse(localStorage.getItem("searchCityList")) || [];
-  
+
 }
 
 //////////////////////////////////////////
@@ -47,7 +48,7 @@ function loadSearchHistory() {
 function updateCityButtons() {
   var searchHistoryEl = document.getElementById('search-history');
   searchHistoryEl.innerHTML = '';
- 
+
   for (var i = 0; i < searchCityList.length; i++) {
     // console.log(`appending button ${i}: ${searchCityList[i]}`);
     var newButton = document.createElement("button");
@@ -59,15 +60,11 @@ function updateCityButtons() {
 //////////////////////////////////////////
 // FUNCTION addCityName
 // -Adds city to search history if not already there. 
-// -saves last search in global variable so it
-// can be used to update search button text field
 //
 function addCityName(city) {
   if (!searchCityList.includes(city)) {
     console.log(`adding city ${city}`);
     searchCityList.push(city);
-    // console.log(` ${searchCityList}`);
-    updateCityButtons();
   }
 }
 /////////////////////////////////////////////
@@ -78,78 +75,103 @@ function addCityName(city) {
 // --and five day forecast
 //
 function displayWeatherData(city) {
-  lastSearch = city;
-  saveSearchHistory ();
-  // Get current weather conditions
-  // and the lat long of the city
+  // clear  city name error message
+  var errMsgDivEL = document.getElementById("city-error-msg");
+  errMsgDivEL.innerHTML = '';
+  // call openweather with city name
+  // to get the lat/long
   fetch(
     openWeatherAPI +
     city +
     weatherUnits +
     apiKey
 
-  ).then(function (resp) {
-    return resp.json();
+  ).then(function (response) {
+    if (response.ok) {
+      cityNameOK = true;
+      //save this city info
+      lastSearch = city;
+      searchCityEl = document.getElementById('search-city');
+      searchCityEl.value = lastSearch;
 
+      addCityName(city);
+      saveSearchHistory();
+      updateCityButtons();
+      return response.json();
+    } else {
+      cityNameOK = false;
+      searchCityList.pop; 
+      // TBD: refine error processing
+      var errMsgDivEL = document.getElementById("city-error-msg");
+      var errMsgEl = document.createElement("h3");
+      errMsgEl.class = "error";
+      errMsgEl.textContent = "Please enter a valid city name";
+      errMsgDivEL.appendChild(errMsgEl);y
+      
+    }
   }).then(function (data) {
-    // extract longitude and latitude from first call
-    var longitude = data.coord.lon;
-    var latitude = data.coord.lat;
-    // then use OneCallAPI for everything else
-    fetch(openWeatherOneCallAPI +
-      "&lat=" + latitude +
-      "&lon=" + longitude +
-      weatherUnits +
-      excludeString +
-      apiKey
+    if (cityNameOK) {
+      // extract longitude and latitude from first call
+      var longitude = data.coord.lon;
+      var latitude = data.coord.lat;
+      // then use OneCallAPI for everything else
+      fetch(openWeatherOneCallAPI +
+        "&lat=" + latitude +
+        "&lon=" + longitude +
+        weatherUnits +
+        excludeString +
+        apiKey
 
-    ).then(function (resp2) {
-      return resp2.json();
+      ).then(function (resp2) {
+        return resp2.json();
 
-    }).then(function (data) {
-      //now we have all our data and can start to display
-      console.log(data);
-      // var searchCity = document.getElementById('searchcity').value;
-      //  selected city
-      var displayCityEl = document.getElementById('display-city');
-      displayCityEl.textContent = city;
-      //  today's date
-      var dateTodayEl = document.getElementById("date-today");
-      dateTodayEl.textContent = ` (${today}) `;
-      //  weather icon
-      var cwIconEl = document.getElementById("current-weather-icon");
-      cwIconEl.setAttribute("src", `${iconURL}` + `${data.current.weather[0].icon}.png`);
-      //  temperature
-      var cTempEl = document.getElementById("current-temp");
-      cTempEl.textContent = data.current.temp;
-      // humidity
-      var cHumidityEl = document.getElementById("current-humidity");
-      cHumidityEl.textContent = data.current.humidity;
-      // wind speed
-      var cWindSpeedEl = document.getElementById("current-wind-speed");
-      cWindSpeedEl.textContent = data.current.wind_speed;
-      // UV Index
-      var cUViEl = document.getElementById("current-uvi");
-      cUViEl.textContent = data.current.uvi;
+      }).then(function (data) {
+        //now we have all our data and can start to display
+        console.log(data);
+        //  selected city
+        var displayCityEl = document.getElementById('display-city');
+        displayCityEl.textContent = city;
+        //  today's date
+        var dateTodayEl = document.getElementById("date-today");
+        dateTodayEl.textContent = ` (${today}) `;
+        //  weather icon
+        var cwIconEl = document.getElementById("current-weather-icon");
+        cwIconEl.setAttribute("src", `${iconURL}` + `${data.current.weather[0].icon}.png`);
+        //  temperature
+        var cTempEl = document.getElementById("current-temp");
+        cTempEl.textContent = data.current.temp;
+        // humidity
+        var cHumidityEl = document.getElementById("current-humidity");
+        cHumidityEl.textContent = data.current.humidity;
+        // wind speed
+        var cWindSpeedEl = document.getElementById("current-wind-speed");
+        cWindSpeedEl.textContent = data.current.wind_speed;
+        // UV Index
+        var cUViEl = document.getElementById("current-uvi");
+        cUViEl.textContent = data.current.uvi;
 
-      // build the forecast cards
+        // build the forecast cards
 
-      for (var i = 1; i <= 5; i++) {
-        // date
-        var new_date = moment(today, "MM-DD-YYYY").add(i, 'days').format("MM-DD-YYYY");
-        var cardId = `today-plus-` + `${i}`;
-        var cardEl = document.getElementById(cardId);
-        var cardTitleEl = cardEl.querySelector(".card-date");
-        cardTitleEl.textContent = new_date;
-        var cardIconEl = cardEl.querySelector(".card-icon");
-        cardIconEl.setAttribute("src", `${iconURL}` + `${data.daily[i].weather[0].icon}.png`);
-        var cardTempEl = cardEl.querySelector(".card-temp");
-        cardTempEl.textContent = data.daily[i].temp.day;
-        var cardHumidityEl = cardEl.querySelector(".card-humidity");
-        cardHumidityEl.textContent = data.daily[i].humidity;
-      }
-    });
+        for (var i = 1; i <= 5; i++) {
+          // date
+          var new_date = moment(today, "MM-DD-YYYY").add(i, 'days').format("MM-DD-YYYY");
+          var cardId = `today-plus-` + `${i}`;
+          var cardEl = document.getElementById(cardId);
+          var cardTitleEl = cardEl.querySelector(".card-date");
+          cardTitleEl.textContent = new_date;
+          var cardIconEl = cardEl.querySelector(".card-icon");
+          cardIconEl.setAttribute("src", `${iconURL}` + `${data.daily[i].weather[0].icon}.png`);
+          var cardTempEl = cardEl.querySelector(".card-temp");
+          cardTempEl.textContent = data.daily[i].temp.day;
+          var cardHumidityEl = cardEl.querySelector(".card-humidity");
+          cardHumidityEl.textContent = data.daily[i].humidity;
+        }
+      }).catch(function () {
+        alert("Network Error.");
+      });
+    }
   });
+
 }
 
 ////////////////////////////////
@@ -163,20 +185,19 @@ loadSearchHistory();
 // initialize the display with the last
 // city searched and add search history
 var searchCityEl = document.getElementById('search-city');
-updateCityButtons();
 searchCityEl.value = lastSearch;
+updateCityButtons();
 // searchCityEl.textContent = lastSearch;
 displayWeatherData(lastSearch);
 
 
-// EVENT LISTENER ON SEARCH BUTTON
+// EVENT LISTENER ON SEARCH INPUT TEXT
 // on click -get city - convert to uppercase
 // - add to search history - display weather 
 var cityEl = document.getElementById("search-button");
 cityEl.addEventListener('click', function () {
   searchCity = document.getElementById('search-city').value;
   searchCity = searchCity.toUpperCase();
-  addCityName(searchCity);
   displayWeatherData(searchCity);
 });
 
@@ -187,7 +208,6 @@ var cityListEl = document.getElementById("search-history");
 cityListEl.addEventListener('click', event => {
   var searchCityEl = event.target;
   searchCity = searchCityEl.textContent;
-  // searchCity = searchCity.toUpperCase();
   displayWeatherData(searchCity);
 
 
